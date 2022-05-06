24dbe570d665d347e7c9157afe5386b0f7c46635
Abhi <abhiran6264@gmail.com>
2022-05-06 18:13:24 +0530

    Refactored the program

--- src/com/java/interviewprog/StringReversal.java
+++ src/com/java/interviewprog/StringReversal.java
@@ -5,22 +5,19 @@ import java.util.Scanner;
 public class StringReversal {
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the string ");
         String next = scanner.nextLine();
-
-       // String next ="My name is abhijeet";
         reverseString(next);
     }
 
-    private static void reverseString(String reverse){
-        String reversedString = new String();
-        for(int i=reverse.length()-1; i>=0;i--){
+    private static void reverseString(String reverse) {
+        String reversedString = "";
+        for (int i = reverse.length() - 1; i >= 0; i--) {
             char c = reverse.charAt(i);
-            reversedString = reversedString+c;
+            reversedString = reversedString + c;
         }
-
-        System.out.println(" the reversed String is "+reversedString);
+        System.out.println(" the reversed String is " + reversedString);
 
     }
 }
