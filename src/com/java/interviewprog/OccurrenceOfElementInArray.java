59d62631c596959b642ed5832a7c2a13f76e1ae0
Abhi <abhiran6264@gmail.com>
2022-05-08 00:13:56 +0530

    Occurrence of element in an array

---
+++ src/com/java/interviewprog/OccurrenceOfElementInArray.java
@@ -0,0 +1,26 @@
+package com.java.interviewprog;
+
+import java.util.*;
+
+public class OccurrenceOfElementInArray {
+    public static void main(String[] args) {
+
+        int[] array = {1, 2, 3, 4, 56, 78, 1, 2, 2, 3, 78, 1, 1, 11, 4};
+        HashMap<Integer, Integer> myHashmap = new HashMap<>();
+
+        Arrays.stream(array).forEach(a -> {
+            if (myHashmap.containsKey(a)) {
+                myHashmap.put(a, myHashmap.get(a) + 1);
+            } else {
+                myHashmap.put(a, 1);
+            }
+        });
+
+        myHashmap.forEach((key, value) -> System.out.println(" the element " + key + " has " + value + " occurrence "));
+
+    }
+}
+
+
+
+
