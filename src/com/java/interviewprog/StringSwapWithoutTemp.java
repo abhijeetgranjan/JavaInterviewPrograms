f3279c5c1f47ec5700d2d72246524a6cd2b95d54
Abhi <abhiran6264@gmail.com>
2022-05-07 22:28:24 +0530

    Swapping of two strings without third string

---
+++ src/com/java/interviewprog/StringSwapWithoutTemp.java
@@ -0,0 +1,17 @@
+package com.java.interviewprog;
+
+public class StringSwapWithoutTemp {
+    public static void main(String[] args) {
+        String first ="This is first string ";
+        String second="This is second string";
+        swapString(first,second);
+    }
+
+    private static void swapString(String first, String second){
+        System.out.println("the first string is "+ first+" and second string is "+second+ " before swap ");
+        first= first+second;
+        second= first.substring(0,first.length()-second.length());
+        first= first.substring(second.length(),first.length());
+        System.out.println("the first string is "+ first+" and second string is "+second+ " after swap ");
+    }
+}
