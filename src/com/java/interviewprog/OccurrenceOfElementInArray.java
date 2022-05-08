01edce44019762a609160c057e568ef3b8d975ab
Abhi <abhiran6264@gmail.com>
2022-05-08 20:50:07 +0530

    Sorting of element according to occurrences

--- src/com/java/interviewprog/OccurrenceOfElementInArray.java
+++ src/com/java/interviewprog/OccurrenceOfElementInArray.java
@@ -17,7 +17,15 @@ public class OccurrenceOfElementInArray {
         });
 
         myHashmap.forEach((key, value) -> System.out.println(" the element " + key + " has " + value + " occurrence "));
+        SortElementAccordingToOccurrences sortHashmap = new SortElementAccordingToOccurrences(myHashmap);
+        List<Integer> collect = new ArrayList<>(myHashmap.keySet());
+        collect.sort(sortHashmap);
 
+
+        System.out.print(" element with min to max occurrences");
+        for (Integer integer : collect) {
+            System.out.print("  " + integer);
+        }
     }
 }
 
