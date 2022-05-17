53c4874fe0d99007cb212a4aff887310800f4f65
Abhi <abhiran6264@gmail.com>
2022-05-17 22:40:21 +0530

    Three Sum GFG

---
+++ src/com/java/interviewprog/ThreeSum.java
@@ -0,0 +1,43 @@
+package com.java.interviewprog;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Find a triplet that sum to a given value
+ * Input: array = {12, 3, 4, 1, 6, 9}, sum = 24;
+ * Output: 12, 3, 9
+ */
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = new int[]{12, 3, 4, 1, 6, 9};
+        int target = 24;
+
+        for (int i = 0; i < nums.length; i++) {
+            int currentTarget = target - nums[i];
+            int[] value = twoSum(nums, currentTarget,i );
+            if (value != null) {
+                System.out.println(" the triplets  are " + nums[i] + " and " + value[0] + " and " + value[1]);
+            }
+
+        }
+    }
+
+    public static int[] twoSum(int[] array, int target, int currentIndex) {
+        int[] indexes = new int[2];
+        Set<Integer> mySet = new HashSet<>();
+        for (int i = currentIndex+1; i < array.length; i++) {
+            int missingNo = target - array[i];
+            if (mySet.contains(missingNo)) {
+                indexes[0] = missingNo;
+                indexes[1] = array[i];
+                return indexes;
+            } else {
+                mySet.add(array[i]);
+            }
+
+        }
+        return null;
+    }
+}
