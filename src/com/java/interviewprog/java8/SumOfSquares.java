263268bcab26658d6b65f3fc99600fb5d3b240fa
Abhijeet Ranjan <100616723+abhijeetgranjan@users.noreply.github.com>
2022-08-12 23:58:21 +0530

    Update SumOfSquares.java

--- src/com/java/interviewprog/java8/SumOfSquares.java
+++ src/com/java/interviewprog/java8/SumOfSquares.java
@@ -6,7 +6,6 @@ public class SumOfSquares {
     public static void main(String[] args) {
 
         int[] array = {2, 3, 1, 4, 5, 6, 7, 8, 9, 10};
-        int sum =0;
 
         int reduce = Arrays.stream(array).map(n -> n * n).reduce(0, Integer::sum);
         System.out.println(reduce);
