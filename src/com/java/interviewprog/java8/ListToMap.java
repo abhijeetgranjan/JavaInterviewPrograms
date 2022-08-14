929a3b84e10cc61b22630c08a253f44c3ce72ac9
Abhi <abhiran6264@gmail.com>
2022-08-14 23:29:44 +0530

    List To Map

---
+++ src/com/java/interviewprog/java8/ListToMap.java
@@ -0,0 +1,40 @@
+package com.java.interviewprog.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class ListToMap {
+    public static void main(String[] args) {
+
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1,"abhijeet"));
+        students.add(new Student(2,"ranjan"));
+
+        Map<Integer,String> hashmap = students.stream().collect(Collectors.toMap(Student::getId,Student::getName));
+
+        for (Map.Entry<Integer,String> integerStringMap :hashmap.entrySet()){
+            System.out.println(" the id is " +integerStringMap.getKey() +" and the value is "+integerStringMap.getValue());
+        }
+    }
+}
+
+
+class  Student{
+    private  int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
