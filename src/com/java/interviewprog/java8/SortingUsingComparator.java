e55ea02a2cc5a62f1e34715824b470c08f795c50
Abhi <abhiran6264@gmail.com>
2022-09-10 23:22:17 +0530

    Sorting using comparator

--- src/com/java/interviewprog/java8/SortingUsingComparator.java
+++ src/com/java/interviewprog/java8/SortingUsingComparator.java
@@ -2,11 +2,7 @@ package com.java.interviewprog.java8;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.Comparator.reverseOrder;
 
 public class SortingUsingComparator {
     public static void main(String[] args) {
