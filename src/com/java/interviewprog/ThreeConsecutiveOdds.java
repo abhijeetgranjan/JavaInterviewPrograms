f9f1c9ac70aa1f8ec7a2b4a9651525f95e97d5d7
Abhi <abhiran6264@gmail.com>
2024-02-02 23:39:29 +0530

    1550 ThreeConsecutiveOdds leetcode

--- src/com/java/interviewprog/ThreeConsecutiveOdds.java
+++ src/com/java/interviewprog/ThreeConsecutiveOdds.java
@@ -9,6 +9,7 @@ public class ThreeConsecutiveOdds {
 
     public boolean threeConsecutiveOdds(int[] arr) {
 
+
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0) {
